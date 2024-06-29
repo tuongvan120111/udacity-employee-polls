b6fd4c6a47e2798336ef94d8b166f55bbe2e3eec
tuongvan120111 <tuongvan120111@gmail.com>
2024-06-29 14:31:23 +0700

    Update config for babel

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   presets: ["@babel/preset-env", "@babel/preset-react"],
+  plugins: [
+    "@babel/plugin-proposal-private-property-in-object",
+    // Add other necessary plugins here
+  ],
 };
